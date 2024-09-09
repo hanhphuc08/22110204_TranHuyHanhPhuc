582ffd458333b6f5b57a984765c54e064e08178a
hanhphuc08 <22110204@student.hcmute.edu.vn>
2024-09-09 14:28:27 +0700

    Week 2-3 ConnectDB

---
+++ src/main/java/phucthh/Config/DBConnectSql.java
@@ -0,0 +1,29 @@
+package phucthh.Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnectSql {
+	private final String serverName = "LAPTOP-7596HE42\\MSSQLSERVER01";
+	private final String dbName = "LaptrinhWeb";
+	private final String portNumber = "1433";
+	private final String instance = "";
+	private final String userID = "sa";
+	private final String password = "123456";
+
+	public Connection getConnection() throws Exception {
+		String url = "jdbc:sqlserver://" + serverName + "\\" + instance + ":" + portNumber + ";databaseName=" + dbName;
+		if (instance == null || instance.trim().isEmpty())
+			url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		return DriverManager.getConnection(url, userID, password);
+	}
+
+	public static void main(String[] args) {
+		try {
+			System.out.println(new DBConnectSql().getConnection());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
